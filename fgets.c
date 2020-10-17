233e3a89c3a4819d00546bddffd99ef6f9700400
Abhyudya Bajpai <42513946+Abhyudyabajpai@users.noreply.github.com>
2020-10-17 22:06:48 +0530

    Input string using fgets

---
+++ fgets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    char n[20];
+    printf("Enter the numbers:\n");
+    fgets(n,20,stdin);
+    printf("the string is %s",n);
+    
+    
+    
+	return 0;
+} 
