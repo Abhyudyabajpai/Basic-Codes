19b5d2af34b31c09ce003b6908322720ba1ad7d3
Abhyudya Bajpai <42513946+Abhyudyabajpai@users.noreply.github.com>
2020-10-24 14:02:52 +0530

    Create addition_of_2d_array.c

---
+++ addition_of_2d_array.c
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+
+int main()
+{
+int m, n, c, d, first[10][10], second[10][10], sum[10][10];
+printf("Enter the number of rows and columns of Array(2D)\n");
+scanf("%d%d", &m, &n);
+printf("Enter the elements of first Array\n");
+
+for (c = 0; c < m; c++)
+for (d = 0; d < n; d++)
+scanf("%d", &first[c][d]);
+
+printf("Enter the elements of second Array\n");
+
+for (c = 0; c < m; c++)
+for (d = 0 ; d < n; d++)
+scanf("%d", &second[c][d]);
+
+printf("Sum of Arrays:-\n");
+
+for (c = 0; c < m; c++) {
+for (d = 0 ; d < n; d++) {
+sum[c][d] = first[c][d] + second[c][d];
+printf("%d\t", sum[c][d]);
+}
+printf("\n");
+}
+
+}
